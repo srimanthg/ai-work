343feecb2b5fc601af08a2d65a33375e3f380381
Srimanth Gunturi <srimanth_gunturi@apple.com>
2026-05-03 17:21:43 -0700

    Added interrupt mechanism for user questions

--- agents/learn/lanngraph_official/calculator_agent.py
+++ agents/learn/lanngraph_official/calculator_agent.py
@@ -14,7 +14,10 @@ from langchain.tools import tool
 from langchain_openai import ChatOpenAI
 from langchain_core.messages import SystemMessage, HumanMessage, ToolMessage
 from langgraph.graph import MessagesState, StateGraph, START, END
+from langgraph.types import Command, interrupt
+from langgraph.checkpoint.memory import MemorySaver
 from pathlib import Path
+from typing import Literal
 
 import logging
 
@@ -70,6 +73,21 @@ llm = ChatOpenAI(model="gpt-5")
 llm = llm.bind_tools(tools=tools)
 
 
+def ask_solve_problem(state: MessagesState) -> Command:
+    user_input = interrupt(
+        {
+            "interrupt": "ask",
+            "question": "Do you want to solve a Math problem (no/[question]?",
+        }
+    )
+    if "no" == user_input.lower():
+        return Command(goto=END)
+    else:
+        return Command(
+            goto="node_llm_call", update={"messages": [HumanMessage(user_input)]}
+        )
+
+
 def node_llm_call(state: MessagesState):
     logging.info(f"LLM")
     return {
@@ -98,39 +116,40 @@ def node_tool_invoke(state: dict):
     return {"messages": results}
 
 
-def evaluate(state: MessagesState):
+def has_tool_calls(state: MessagesState):
     last_message = state["messages"][-1]
     if last_message.tool_calls:
         return "node_tool_invoke"
-    return END
+    return "ask_solve_problem"
 
 
 #
 # Agent state
 #
 calculator_graph = StateGraph(state_schema=MessagesState)
+calculator_graph.add_node("ask_solve_problem", ask_solve_problem)
 calculator_graph.add_node("node_llm_call", node_llm_call)
 calculator_graph.add_node("node_tool_invoke", node_tool_invoke)
-calculator_graph.add_edge(START, "node_llm_call")
+calculator_graph.add_edge(START, "ask_solve_problem")
 calculator_graph.add_conditional_edges(
-    "node_llm_call", evaluate, ["node_tool_invoke", END]
+    "node_llm_call", has_tool_calls, ["node_tool_invoke", "ask_solve_problem"]
 )
 calculator_graph.add_edge("node_tool_invoke", "node_llm_call")
 
-calculator_agent = calculator_graph.compile()
+calculator_agent = calculator_graph.compile(checkpointer=MemorySaver())
 Path("calculator-agent.png").write_bytes(
     calculator_agent.get_graph().draw_mermaid_png()
 )
 
-calculator_response = calculator_agent.invoke(
-    # {"messages": [HumanMessage(content="What is (2+3)x5")]}
-    # {"messages": [HumanMessage(content="What is ((2+3)x5)/5")]}
-    {
-        "messages": [
-            HumanMessage(content="What is 3 and 4 added, and then mulipled by 8?")
-        ]
-    }
-)
+config = {"configurable": {"thread_id": "test1"}}
+calculator_response = calculator_agent.invoke(input={}, config=config)
+while "__interrupt__" in calculator_response:
+    if calculator_response["__interrupt__"][0].value["interrupt"] == "ask":
+        asked = input(calculator_response["__interrupt__"][0].value["question"])
+        calculator_response = calculator_agent.invoke(
+            Command(resume=asked), config=config
+        )
+
 calculator_response_messages = calculator_response["messages"]
 logging.info(f"{len(calculator_response_messages)} messages")
 for i, message in enumerate(calculator_response_messages):
